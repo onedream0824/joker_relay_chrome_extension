2804d62e2e0029e1f99c576950c20d8ec08577f3
onedream0824 <onedream082498.gmail.com>
2024-09-05 03:41:45 -0400

    fix autobook feature and stem time

--- src/background.ts
+++ src/background.ts
@@ -57,41 +57,35 @@ function start(
   const sleep = (ms: number) =>
     new Promise((resolve) => setTimeout(resolve, ms));
 
-  const button = document.querySelector(
-    'button[mdn-popover-offset="-8"]'
-  ) as HTMLButtonElement;
-
-  if (button) {
-    button.click();
-  } else {
-    console.error("Button not found!");
-  }
-
   refreshPage = window.setInterval(async () => {
-    const count = (document
+    const count = document
       .querySelector(".search-results-summary__result-summary--show p")
-      ?.textContent?.split(" ")[3]) as string;
+      ?.textContent?.split(" ")[3] as string;
 
-    const resultSummaryPanel = document.querySelector(".css-ftr0v1")?.querySelectorAll('.css-1ehu6yl')[1] as HTMLElement;
+    const resultSummaryPanel = document
+      .querySelector(".css-ftr0v1")
+      ?.querySelectorAll(".css-1ehu6yl")[1] as HTMLElement;
 
-    const loadCards = resultSummaryPanel.querySelectorAll(".load-card")
-    const highlightCards = resultSummaryPanel.querySelectorAll(".wo-card-header--highlighted");
-    
+    const loadCards = resultSummaryPanel.querySelectorAll(".load-card");
+    const highlightCards = resultSummaryPanel.querySelectorAll(
+      ".wo-card-header--highlighted"
+    );
+
     const matched_loads = [...loadCards, ...highlightCards];
 
-    console.log(loadCards.length, highlightCards.length, matched_loads.length)
+    console.log(loadCards.length, highlightCards.length, matched_loads.length);
 
     for (let index = 0; index < Number(count); index++) {
       if (Number(count) <= index) return;
 
       const page_start_time =
-        (matched_loads[index].getElementsByClassName(
+        matched_loads[index].getElementsByClassName(
           "wo-card-header__components"
-        )[1]?.textContent) || "";
+        )[1]?.textContent || "";
 
-      const page_stop: string | null = matched_loads[
-        index
-      ].getElementsByClassName("css-kabd3k")[1].textContent;
+      const page_stop: string | null =
+        matched_loads[index].getElementsByClassName("css-kabd3k")[1]
+          .textContent;
 
       const page_payout: string | undefined = (
         matched_loads[index].getElementsByClassName("wo-total_payout")[0] ||
@@ -124,77 +118,85 @@ function start(
         page_rate,
         Number.parseFloat(rate) <= Number.parseFloat(page_rate || "0")
       );
+      if (autoBook) {
+        if (
+          autoBook &&
+          Number.parseFloat(payout) <= Number.parseFloat(page_payout || "0") &&
+          Number.parseFloat(rate) <= Number.parseFloat(page_rate || "0") &&
+          Number.parseFloat(stop) <= Number.parseFloat(page_stop || "0")
+        ) {
+          const currentYear = new Date().getFullYear();
+          const startDate = new Date(
+            `${page_start_time.replace("BST", "+0100")} ${currentYear}`
+          );
+          const currentDate = new Date();
+          const leftMinutes =
+            (startDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60);
 
-      if (
-        autoBook &&
-        Number.parseFloat(payout) <= Number.parseFloat(page_payout || "0") &&
-        Number.parseFloat(rate) <= Number.parseFloat(page_rate || "0") &&
-        Number.parseFloat(stop) <= Number.parseFloat(page_stop || "0")
-      ) {
-        const currentYear = new Date().getFullYear();
-        const startDate = new Date(
-          `${page_start_time.replace("BST", "+0100")} ${currentYear}`
-        );
-        const currentDate = new Date();
-        const leftMinutes =
-          (startDate.getTime() - currentDate.getTime()) / (1000 * 60 * 60);
+          console.log("Left mins: ", leftMinutes, stem);
 
-        console.log("Left mins: ", leftMinutes, stem);
+          if (leftMinutes < Number(stem)) {
+            const open_button = matched_loads[index].getElementsByClassName(
+              "css-e9glob"
+            )[0] as HTMLElement;
 
-        if (leftMinutes > Number(stem)) {
-          const open_button = matched_loads[index].getElementsByClassName(
-            "css-e9glob"
-          )[0] as HTMLElement;
+            open_button.click();
 
-          open_button.click();
+            await sleep(500);
 
-          await sleep(500);
+            const selectedWorkSheet = document.getElementById(
+              "selected-work-sheet"
+            ) as HTMLElement;
 
-          const selectedWorkSheet = (document.getElementById(
-            "selected-work-sheet"
-          )) as HTMLElement;
+            console.log("Select: ", selectedWorkSheet);
 
-          console.log("Select: ", selectedWorkSheet);
+            if (selectedWorkSheet) {
+              const bookBtn = selectedWorkSheet.querySelector(
+                "button.wo-book-button.css-1lpvuz4"
+              ) as HTMLButtonElement;
 
-          if (selectedWorkSheet) {
-            const bookBtn = (selectedWorkSheet.querySelector(
-              "button.wo-book-button.css-1lpvuz4"
-            )) as HTMLButtonElement;
+              console.log("Book btn: ", bookBtn);
 
-            console.log("Book btn: ", bookBtn);
+              bookBtn.click();
 
-            bookBtn.click();
+              // const confirmNoBtn = selectedWorkSheet.querySelector(
+              //   "button.css-1r6inv4"
+              // ) as HTMLButtonElement;
+              // confirmNoBtn.click();
 
-            // const confirmNoBtn = selectedWorkSheet.querySelector(
-            //   "button.css-1r6inv4"
-            // ) as HTMLButtonElement;
-            // confirmNoBtn.click();
+              // Click confirm btn
+              // const confirmBtn = selectedWorkSheet.querySelector('button.css-n0loux') as HTMLButtonElement;
+              // confirmBtn.click();
 
-            // Click confirm btn
-            // const confirmBtn = selectedWorkSheet.querySelector('button.css-n0loux') as HTMLButtonElement;
-            // confirmBtn.click();
+              if (refreshPage !== null) {
+                clearInterval(refreshPage);
+                refreshPage = null;
 
-            if (refreshPage !== null) {
-              clearInterval(refreshPage);
-              refreshPage = null;
+                chrome.runtime.sendMessage({ action: "RefreshStopped" });
+              }
 
-              chrome.runtime.sendMessage({ action: "RefreshStopped" });
+              break;
             }
-
-            break;
           }
         }
+        const button = document.querySelector(
+          'button[mdn-popover-offset="-8"]'
+        ) as HTMLButtonElement;
+        if (button) {
+          button.click();
+        } else {
+          console.error("Button not found!");
+        }
+      } else {
+        const button = document.querySelector(
+          'button[mdn-popover-offset="-8"]'
+        ) as HTMLButtonElement;
+        if (button) {
+          button.click();
+        } else {
+          console.error("Button not found!");
+        }
       }
-    }
-
-    const button = document.querySelector(
-      'button[mdn-popover-offset="-8"]'
-    ) as HTMLButtonElement;
-
-    if (button) {
-      button.click();
-    } else {
-      console.error("Button not found!");
     }
   }, refreshTimeNum);
 }
